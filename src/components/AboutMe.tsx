70ee9ae637564ed88d5e6dacfc7c9c22f992c741
Aysha Hamna <ayshahamna57@gmail.com>
2024-05-20 00:04:44 +0530

    added type animation for my name

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,10 @@
 import Techstack from "./Techstack";
+import {ReactTyped} from "react-typed";
 
 const AboutMe = () => {
   return (
     <div className="py-20">
-      <h1>Hey, I'm Hamna</h1>
+      <h1><ReactTyped strings={["Hey, I'm Hamna"]} typeSpeed={100}/></h1>
       <p>
         Front-end developer. Eager to contribute my technical expertise and
         collaborate on innovative projects in a dynamic environment.
