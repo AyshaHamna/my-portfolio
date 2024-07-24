4ac5a0b4416c2216102ff5b3e194b2d7287d88ab
AyshaHamna <ayshahamna57@gmail.com>
2024-07-24 23:56:50 +0530

    added "AI chat with PDF" project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,34 +9,52 @@ import {
 import { Button } from "./ui/button";
 import { Github } from "lucide-react";
 
+const projects = [
+  {
+    name: "HirelyAi",
+    techStack: "Reactjs, Nodejs, openAi, Mongodb",
+    description:
+      "Job Portal powered by AI, with applicant rating system based on responses.",
+    githubRepo: "https://github.com/AyshaHamna/ai-job-frontend",
+    link: "https://aidf-front-end-hamna.netlify.app/",
+  },
+  {
+    name: "AI Chat with PDF",
+    techStack: "Nextjs, Reactjs, Clerk",
+    description:
+      "An ongoing project that integrates PDF interaction capabilities within a chat interface, allowing users to seamlessly chat and manage PDFs.",
+    githubRepo: "https://github.com/AyshaHamna/chat-with-pdf-challenge",
+    link: "https://chat-with-pdf-challenge-hamna.vercel.app/",
+  },
+];
+
 const ProjectCard = () => {
   return (
-    <div className="m-5">
-      <Card className="rounded-xl">
-        <CardHeader>
-          <CardTitle>HirelyAi</CardTitle>
-          <CardDescription>Reactjs, Nodejs, openAi, Mongodb</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>
-            Job Portal powered by AI, with applicant rating system based on
-            responses.
-          </p>
-        </CardContent>
-        <CardFooter className="flex gap-4">
-          <a href="https://github.com/AyshaHamna/ai-job-frontend">
-            <Button className="rounded" variant="secondary">
-              <Github />
-              Code
-            </Button>
-          </a>
-          <a href="https://aidf-front-end-hamna.netlify.app/">
-            <Button className="rounded" variant="outline">
-              Live Demo
-            </Button>
-          </a>
-        </CardFooter>
-      </Card>
+    <div className="m-5 flex flex-col gap-5">
+      {projects.map((project) => (
+        <Card className="rounded-xl" key={project.name}>
+          <CardHeader>
+            <CardTitle>{project.name}</CardTitle>
+            <CardDescription>{project.techStack}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>{project.description}</p>
+          </CardContent>
+          <CardFooter className="flex gap-4">
+            <a href={project.githubRepo}>
+              <Button className="rounded" variant="secondary">
+                <Github />
+                Code
+              </Button>
+            </a>
+            <a href={project.link}>
+              <Button className="rounded" variant="outline">
+                Live Demo
+              </Button>
+            </a>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 };
